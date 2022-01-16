abe4ad0362af7a90eec96f78c1c8f62dfdc84eed
xcanwin <xcanwin@gmail.com>
2022-01-17 00:22:18 +0800

    解决了小白不清楚OOB请求类型的痛点, 我说一个数...... TCP与UDP并存.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 ﻿# XNetcat
 ## 简介
-- 基于Python的Netcat, 用于外带数据攻击.
+- 基于Python的Netcat服务器, 用于接收OOB与SSRF请求, 兼容TCP与UDP.
 
 ## 作用
-- 类似于 ncat -l -k -v -o netcat.log --append-output -p 4444
-- 监听端口, 获取接收到的数据.
-- 保持端口监听, 可收到客户端多次新建的请求.
+- 监听端口, 输出接收到的数据.
+- 兼容TCP与UDP.
+- 功能类似于ncat的TCP服务端 ncat -l -k -v -o netcat.log --append-output -p 4444
+- 同时类似于ncat的UDP服务端 ncat -l -k -v -o netcat.log --append-output -p 4444 -u
+- 时刻监听同一端口, 可不限制地接收客户端新建的请求.
 - 收到请求后立即断开, 避免占用靶机的程序线程和网络连接.
 - 记录历史请求, 保存请求原文.
 
@@ -15,4 +17,6 @@
 - 跨平台.
 - 解决了内网环境无法下载ncat的问题.
 - 解决了ncat命令过于冗余的问题.
-- 解决了ncat监听模式下需要频繁ctrl+c才能终止oob连接的问题.
+- 解决了ncat监听模式下需要频繁ctrl+c才能终止OOB连接的问题.
+- 兼容Python2和Python3.
+- 解决了小白不清楚OOB请求类型的痛点, 我说一个数...... TCP与UDP并存.
--- xnetcat.py
+++ xnetcat.py
@@ -3,26 +3,37 @@ import socket, threading
 server_host = '0.0.0.0'
 server_port = 4444
 
-def listen(server_host, server_port):
-    print('XNETCAT: Listening on %s:%s' % (server_host, server_port))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def listen(server_host, server_port, server_type):
+    sock = socket.socket(socket.AF_INET, server_type)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((server_host, server_port))
     while True:
-        sock.listen(1)
-        connection, (client_host, client_port) = sock.accept()
-        result_from = '\n\nXNETCAT: Connection from %s:%s\n' % (client_host, client_port)
+        if sock.type == socket.SOCK_STREAM:
+            client_type = 'TCP'
+            sock.listen(1)
+            connection, (client_host, client_port) = sock.accept()
+            get_data1 = connection.recv(1024*1024)
+        elif sock.type == socket.SOCK_DGRAM:
+            client_type = 'UDP'
+            get_data1, (client_host, client_port) = sock.recvfrom(1024*1024)
+        else:
+            client_type = 'OTHER'
+        result_from = '\n\nXNETCAT: %s: Connection from %s:%s\n' % (client_type, client_host, client_port)
         result = result_from.encode()
-        get_data1 = connection.recv(1024*1024)
         result += get_data1 + b'\n'
         print(result.decode('utf-8', 'ignore'))
         open('xnetcat.log', 'ab').write(result)
-        connection.close()
+        if sock.type == socket.SOCK_STREAM:
+            connection.close()
     sock.close()
 
-t = threading.Thread(target=listen, args=(server_host, server_port))
-t.daemon = True
-t.start()
+print('XNETCAT: Listening on %s:%s' % (server_host, server_port))
+t1 = threading.Thread(target=listen, args=(server_host, server_port, socket.SOCK_STREAM))
+t1.daemon = True
+t1.start()
+t2 = threading.Thread(target=listen, args=(server_host, server_port, socket.SOCK_DGRAM))
+t2.daemon = True
+t2.start()
 try:
     while True:
         pass
