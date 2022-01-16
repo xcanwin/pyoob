bcc3f19e23ed5e1620fab18dec6d30b1bbda29eb
xcanwin <xcanwin@gmail.com>
2022-01-16 21:55:02 +0800

    保持端口监听，可收到客户端多次新建的请求。

--- xnetcat.py
+++ xnetcat.py
@@ -1,11 +1,25 @@
-import socket
-port = 61359
-print('WAITING...')
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-sock.bind(('0.0.0.0',port))
-sock.listen(1)
-connection,address = sock.accept()
-print('XNETCAT:')
-print(connection.recv(1024).decode('utf-8','ignore'))
-connection.close()
+import socket, threading
+
+server_host = '0.0.0.0'
+server_port = 61359
+
+def listen(server_host, server_port):
+    print('XNETCAT: Listening on %s:%s' % (server_host, server_port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+    sock.bind((server_host, server_port))
+    sock.listen(5)
+    while True:
+        connection, (client_host, client_port) = sock.accept()
+        print('\n\nXNETCAT: Connection from %s:%s' % (client_host, client_port))
+        print(connection.recv(1024*100).decode('utf-8','ignore'))
+        connection.close()
+
+t = threading.Thread(target=listen, args=(server_host, server_port))
+t.daemon = True
+t.start()
+try:
+    while True:
+        pass
+except:
+    exit('XNETCAT: Exit')
