fe8b27c5f170f9b0c24a18e5ed01b17f9f60d270
xcanwin <xcanwin@gmail.com>
2022-01-16 21:59:17 +0800

    增加日志功能

--- xnetcat.py
+++ xnetcat.py
@@ -1,18 +1,22 @@
 import socket, threading
 
 server_host = '0.0.0.0'
-server_port = 61359
+server_port = 4444
 
 def listen(server_host, server_port):
     print('XNETCAT: Listening on %s:%s' % (server_host, server_port))
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((server_host, server_port))
     sock.listen(5)
     while True:
         connection, (client_host, client_port) = sock.accept()
-        print('\n\nXNETCAT: Connection from %s:%s' % (client_host, client_port))
-        print(connection.recv(1024*100).decode('utf-8','ignore'))
+        result_from = '\n\nXNETCAT: Connection from %s:%s\n' % (client_host, client_port)
+        result = result_from.encode()
+        get_data1 = connection.recv(1024*1024)
+        result += get_data1 + b'\n'
+        print(result.decode('utf-8', 'ignore'))
+        open('xnetcat.log', 'ab').write(result)
         connection.close()
 
 t = threading.Thread(target=listen, args=(server_host, server_port))
