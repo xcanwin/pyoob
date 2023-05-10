696ac95e2ace5c6e32e4f513ab82e9ca720fbb65
未知可行 <xcanwin@gmail.com>
2023-05-10 16:12:21 +0800

    全新升级，解决cpu高占用的bug，支持自定义传参和默认参数

--- xnetcat.py
+++ xnetcat.py
@@ -1,7 +1,7 @@
-import socket, threading
+import socket, threading, argparse
 
-def listen(server_host, server_port, server_type):
-    sock = socket.socket(socket.AF_INET, server_type)
+def listen(server_host, server_port, server_xcanwin, log_file):
+    sock = socket.socket(socket.AF_INET, server_xcanwin)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((server_host, server_port))
     while True:
@@ -19,27 +19,37 @@ def listen(server_host, server_port, server_type):
         result = result_from.encode()
         result += get_data1 + b'\n'
         print(result.decode('utf-8', 'ignore'))
-        open('xnetcat.log', 'ab').write(result)
+        open(log_file, 'ab').write(result)
         if sock.type == socket.SOCK_STREAM:
             connection.close()
     sock.close()
 
-def create_thread(server_host, server_port, server_type):
-    t = threading.Thread(target=listen, args=(server_host, server_port, server_type))
+def create_thread(server_host, server_port, server_xcanwin, log_file):
+    t = threading.Thread(target=listen, args=(server_host, server_port, server_xcanwin, log_file))
     t.daemon = True
     t.start()
 
 def set_thread_tail():
     try:
         while True:
-            pass
+            input('')
     except:
         exit('XNETCAT: Exit')
 
-
-server_host = '0.0.0.0'
-server_port = 4444
-print('XNETCAT: Listening on %s:%s' % (server_host, server_port))
-for server_type in (socket.SOCK_STREAM, socket.SOCK_DGRAM):
-    create_thread(server_host, server_port, server_type)
-set_thread_tail()
+if __name__ == '__main__':
+    description_tips = '''基于Python的轻量级Netcat服务器，用于接收HTTP、OOB、SSRF等多种请求，
+同时监听TCP与UDP两种协议，并且可以持续获取结果，有日志记录功能。'''
+    usage_tips = '''Example:
+  python %(prog)s
+  python %(prog)s -P 4444
+  python %(prog)s -H 0.0.0.0 -P 4444
+  python %(prog)s -H 0.0.0.0 -P 4444 -L xnetcat.log'''
+    parser = argparse.ArgumentParser(description = description_tips, epilog = usage_tips, formatter_class = argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-H', '--server-host', default = '0.0.0.0', help = 'Listen host')
+    parser.add_argument('-P', '--server-port', type=int, default = 4444, help = 'Listen port')
+    parser.add_argument('-L', '--log-file', default = './xnetcat.log', help = 'Log file')
+    options = parser.parse_args()
+    print('XNETCAT: Listening on %s:%s' % (options.server_host, options.server_port))
+    for server_xcanwin in (socket.SOCK_STREAM, socket.SOCK_DGRAM):
+        create_thread(options.server_host, options.server_port, server_xcanwin, options.log_file)
+    set_thread_tail()
