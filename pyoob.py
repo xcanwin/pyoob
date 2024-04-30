1b643fd644d6b9fbb0edffd3d21b18df79a9517d
未知可行 <14187349+xcanwin@users.noreply.github.com>
2024-05-01 02:35:12 +0800

    Update and rename xnetcat.py to pyoob.py

--- pyoob.py
+++ pyoob.py
@@ -15,7 +15,7 @@ def listen(server_host, server_port, server_xcanwin, log_file):
             get_data1, (client_host, client_port) = sock.recvfrom(1024*1024)
         else:
             client_type = 'OTHER'
-        result_from = '\n\nXNETCAT: %s: Connection from %s:%s\n' % (client_type, client_host, client_port)
+        result_from = '\n\nPYOOB: %s: Connection from %s:%s\n' % (client_type, client_host, client_port)
         result = result_from.encode()
         result += get_data1 + b'\n'
         print(result.decode('utf-8', 'ignore'))
@@ -34,22 +34,22 @@ def set_thread_tail():
         while True:
             input('')
     except:
-        exit('XNETCAT: Exit')
+        exit('PYOOB: Exit')
 
 if __name__ == '__main__':
-    description_tips = '''基于Python的轻量级Netcat服务器，用于接收HTTP、OOB、SSRF等多种请求，
+    description_tips = '''基于Python的轻量级NetCat服务器，用于接收HTTP、OOB、SSRF等多种请求，
 同时监听TCP与UDP两种协议，并且可以持续获取结果，有日志记录功能。'''
     usage_tips = '''Example:
   python %(prog)s
   python %(prog)s -P 4444
   python %(prog)s -H 0.0.0.0 -P 4444
-  python %(prog)s -H 0.0.0.0 -P 4444 -L xnetcat.log'''
+  python %(prog)s -H 0.0.0.0 -P 4444 -L pyoob.log'''
     parser = argparse.ArgumentParser(description = description_tips, epilog = usage_tips, formatter_class = argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-H', '--server-host', default = '0.0.0.0', help = 'Listen host')
     parser.add_argument('-P', '--server-port', type=int, default = 4444, help = 'Listen port')
-    parser.add_argument('-L', '--log-file', default = './xnetcat.log', help = 'Log file')
+    parser.add_argument('-L', '--log-file', default = './pyoob.log', help = 'Log file')
     options = parser.parse_args()
-    print('XNETCAT: Listening on %s:%s' % (options.server_host, options.server_port))
+    print('PYOOB: Listening on %s:%s' % (options.server_host, options.server_port))
     for server_xcanwin in (socket.SOCK_STREAM, socket.SOCK_DGRAM):
         create_thread(options.server_host, options.server_port, server_xcanwin, options.log_file)
     set_thread_tail()
